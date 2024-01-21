df2ed73f681eb9c6d018ac7e183c184e7ca676c4
Gogo <progressgogochinda@gmail.com>
2024-01-21 21:13:51 +0100

    line arguement

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,14 +6,14 @@ if __name__ == "__main__":
     length = len(argv)
 
     if length == 1:
-        print("{} arguements.".format(length - 1))
+        print("{:d} arguements.".format(length - 1))
     else:
         numbering = 1
         if length == 2:
-            print(f"{length - 1} arguement:")
+            print(f"{(length - 1):d} arguement:")
         else:
-            print(f"{length - 1} arguements:")
+            print(f"{(length - 1):d} arguements:")
         for i in argv:
             if i is not argv[0]:
-                print(f"{numbering}: {i:s}")
+                print(f"{numbering:d}: {i:s}")
                 numbering += 1
