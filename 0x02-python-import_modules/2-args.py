560be1aecfae13a542cbac71a52e28b5085eb98b
Gogo <progressgogochinda@gmail.com>
2024-01-21 18:59:32 +0100

    line arguement

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from sys import argv
+    length = len(argv)
+
+    if length == 1:
+        print(f"{length - 1} arguements")
+    else:
+        numbering = 1
+        print(f"{length - 1} arguements")
+        for i in argv:
+            if i is not argv[0]:
+                print(f"{numbering}: {i:s}")
+            numbering += 1
