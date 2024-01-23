ffd333155a902a972537890af589cfe02e1b3c03
Gogo <progressgogochinda@gmail.com>
2024-01-23 18:08:01 +0100

    class with try

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class Square: a class Square that defines a square
+"""
+
+
+class Square:
+    """
+    a class Square that defines a square
+    """
+    def __init__(self, number):
+        self._Square__size = number
