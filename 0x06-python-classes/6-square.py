f73c8987a6848316ab66b03d1ad3f971d2693862
Gogo <progressgogochinda@gmail.com>
2024-01-24 13:50:52 +0100

    position and size

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+class Square: a class Square that defines a square by
+"""
+
+
+class Square:
+    """
+    a class Square that defines a square by
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self._Square__size = size
+        self._Gogo = position
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        self._Square__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def position(self):
+        return self._Gogo
+
+    @position.setter
+    def position(self, value):
+        self._Gogo = value
+        if type(position) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        return self._Square__size**2
+
+    def my_print(self):
+        if self._Square__size == 0:
+            print()
+        else:
+            if self._Gogo[1] > 0:
+                print()
+            for i in range(self._Square__size):
+                print(f"{' '  * self._Gogo[0] + '#' * self._Square__size}")
