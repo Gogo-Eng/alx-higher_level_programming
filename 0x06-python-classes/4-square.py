1dba152d8b8ab6ed64574f3093b1ad5a59a3950b
Gogo <progressgogochinda@gmail.com>
2024-01-24 03:00:18 +0100

    property and setter

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+class Square: a class Square that defines a square by
+"""
+
+
+class Square:
+    """
+    a class Square that defines a square by
+    """
+    def __init__(self, size=0):
+        self._Square__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        self._Square__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self._Square__size**2
