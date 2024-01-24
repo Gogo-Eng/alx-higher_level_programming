eba583ab75eb45cf52f0af3a46b79f15c00549e3
Gogo <progressgogochinda@gmail.com>
2024-01-24 15:34:44 +0100

    position and size

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -36,7 +36,10 @@ class Square:
     @position.setter
     def position(self, value):
         self._Gogo = value
-        if type(position) is not int:
+        if (type(position) is not tuple or
+        len(value) != 2 or
+        not all(isinstance(i, int) for i in value) or
+        not all(i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
 
     def area(self):
@@ -45,6 +48,7 @@ class Square:
     def my_print(self):
         if self._Square__size == 0:
             print()
+            return
         elif self._Gogo[1] > 0:
             for i in range(self._Gogo[1]):
                 print()
