b523a6b4cedfd0a09470b5b7e9197d49d0f2ad8c
Gogo-Eng <progressgogochinda@gmail.com>
2024-02-01 01:37:04 +0100

    alot

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,8 +47,8 @@ class Rectangle:
             return 2 * (int(self.__width) + int(self.__height))
 
     def __str__(self):
-        result = []
+        result = ""
         if self.__width > 0 and self.__height > 0:
             for i in range(self.__height):
-                result.append("#" * self.__width)
-        return ''.join(result)
+                result += "#" * self.__width + '\n'
+        return result[:-1]
