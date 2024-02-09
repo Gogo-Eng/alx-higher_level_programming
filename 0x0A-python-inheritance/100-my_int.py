a95f63820d3ee7d3f80568706bca5574304ad72b
Gogo-Eng <progressgogochinda@gmail.com>
2024-02-09 12:48:42 +0100

    a class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+ a class MyInt that inherits from int
+"""
+
 
 class MyInt(int):
+    """
+
+    Args:
+        int: the class we are inheriting from
+    """
 
     def __eq__(self, num):
         return self.real != num
