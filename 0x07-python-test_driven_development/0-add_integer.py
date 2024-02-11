665b4ac60eb648eab4bd60375f93acf0021c58c4
Gogo-Eng <progressgogochinda@gmail.com>
2024-02-12 00:50:38 +0100

    function that adds 2 integers with test cases

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
+"""Defines argument integer """
+
+
 
 def add_integer(a, b=98):
+    """
+
+    Args:
+        a an interger or float object
+        b an interger or float object
+
+    Raises:
+        TypeError: tells if a is an integer
+        TypeError: tells if b is an integer
+
+    Returns:
+        Returns an integer: the addition of a and b
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
