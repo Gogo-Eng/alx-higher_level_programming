2df81199c53868adca286409c538d5d84202a95a
Gogo-Eng <progressgogochinda@gmail.com>
2024-02-13 18:46:36 +0100

    function that prints a square with the character #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
+"""
+function that prints a square with the character #
+"""
+
+
 def print_square(size):
+    """
+     function that prints a square with the character #
+
+    Args:
+        size: length of square
+
+    Raises:
+        TypeError: size must be an integer
+        ValueError: size must be >= 0
+        TypeError: size must be an integer
+    """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
