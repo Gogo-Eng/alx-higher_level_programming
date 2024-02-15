7f7dff60d782070bfc7f71e5f841f29354e2122f
Gogo-Eng <progressgogochinda@gmail.com>
2024-02-15 15:40:47 +0100

    Text indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,6 +2,8 @@
 """
 prints a text with 2 new lines after each of these characters: ., ? and :
 """
+
+
 def text_indentation(text):
     """
     prints a text with 2 new lines after each of these characters: ., ? and :
