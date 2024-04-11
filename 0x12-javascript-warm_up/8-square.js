2fd7a04a12967345750c2df1dab9e504315c649a
Gogo-Eng <progressgogochinda@gamil.com>
2024-04-11 18:30:06 +0100

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+let i, j;
+const x = process.argv[2];
+if (!parseInt(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < Number(x); i++) {
+    let row = '';
+    for (j = 0; j < Number(x); j++) {
+      row += 'X';
+    }
+    console.log(row);
+  }
+}
---
+++ 0x12-javascript-warm_up/hh.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 2; j++)
+        {
+            printf("Gogo");
+        }
+        printf("\n");
+    }
+    return 0;
+}
