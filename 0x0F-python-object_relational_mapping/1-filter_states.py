5635998983493a658c16d877ac310fc1bd516432
Gogo-Eng <progressgogochinda@gamil.com>
2024-04-20 13:54:12 +0100

    Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+list all states
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database = sys.argv[3]
+    db = MySQLdb.connect(host="localhost", user=mysql_username,
+                         passwd=mysql_password, db=database, port=3306)
+
+    mycursor = db.cursor()
+    Q1 = ("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    mycursor.execute(Q1)
+    for l in mycursor:
+        print(l)
+
+    mycursor.close()
+    db.close()
