8f7a246318f37111d4aa41d03845685ba62c18d9
Gogo-Eng <progressgogochinda@gamil.com>
2024-04-22 01:07:18 +0100

    fool gold

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-
-"""list all states corresponding to the argument"""
+"""list all states that are argument, no injection risk"""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-
-    state_name_search = argv[4]
-
-    db = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2],
-                         database=argv[3], port=3306)
-    mycursor = db.cursor()
-
-    mycursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
-                     (state_name_search,))
-    list = mycursor.fetchall()
-    for l in list:
-        print(l)
-
-    mycursor.close()
+    db = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+        (argv[4], ))
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
     db.close()
